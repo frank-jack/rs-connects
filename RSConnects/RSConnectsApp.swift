4abc7179304f872cc9c30c4694f797fcd780f214
Jack Frank <jackfrank1001@gmail.com>
2023-05-08 11:15:49 -0400

    initial commit

---
+++ RSConnects/RSConnectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSConnectsApp.swift
+//  RSConnects
+//
+//  Created by Jack Frank on 5/8/23.
+//
+
+import SwiftUI
+
+@main
+struct RSConnectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
